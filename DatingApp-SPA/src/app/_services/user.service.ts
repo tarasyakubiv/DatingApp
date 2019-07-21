33a80a9c488b6c62c18c83e034f36c6342c0b88d
tarasyakubiv <taras.yakubiv@gmail.com>
2019-07-21 17:49:02 -0400

    End of Section 15

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -17,7 +17,8 @@ export class UserService {
   getUsers(
     page?,
     itemsPerPage?,
-    userParams?
+    userParams?,
+    likesParam?
   ): Observable<PaginatedResult<User[]>> {
     const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<
       User[]
@@ -33,6 +34,15 @@ export class UserService {
       params = params.append("gender", userParams.gender);
       params = params.append("orderBy", userParams.orderBy);
     }
+
+    if (likesParam == "Likers") {
+      params = params.append("likers", "true");
+    }
+
+    if (likesParam == "Likees") {
+      params = params.append("likees", "true");
+    }
+
     return this.http
       .get<User[]>(this.baseUrl + "users", {
         observe: "response",
@@ -68,5 +78,12 @@ export class UserService {
 
   deletePhoto(userId: number, id: number) {
     return this.http.delete(this.baseUrl + "users/" + userId + "/photos/" + id);
+  }
+
+  sendLike(id: number, recipientId: number) {
+    return this.http.post(
+      this.baseUrl + "users/" + id + "/like/" + recipientId,
+      {}
+    );
   }
 }
